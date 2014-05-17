2a3be90a0acf893de5a7f7ca51541c93ddbf8d8a
Vanush Vaswani <vanush@gmail.com>
2014-05-18 02:28:40 +1000

    hope this works :P

--- binary_to_wav.m
+++
@@ -1,7 +0,0 @@
-function binarywav = binary_to_wav(data)
-    d = char(data+48);
-    d1 = bin2dec(d);
-    binarywav = d1./255;
-    binarywav = binarywav - 0.5;
-end
-    
--- wav_to_binary.m
+++
@@ -1,8 +0,0 @@
-function wavbinary = wav_to_binary(filename)
-    [x, Fs] = wavread(filename, 'native');
-    x1 = x - min(x);
-    s = dec2bin(x);
-    wavbinary = double(s) - 48;
-end
-    
-    
