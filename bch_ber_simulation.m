a38ff0b3a9b29b28fb4179375a9ce8c4a54fc79b
unknown <stri1875@W2J546Z1.internal.usyd.edu.au>
2014-05-22 18:10:01 +1000

    Performance increases in simulations - remove the evil loops

--- bch_ber_simulation.m
+++ bch_ber_simulation.m
@@ -1,67 +1,50 @@
 % simulations section 1) part 8)
 % Find the BER over a BCH code
-pValues = 0.001:0.001:10;
+pValues = 0.001:0.001:0.5;
 snrValues = 0.001:0.001:10;
 repetitions = 100;
-messageLength = 16;
 
-BER = zeros(1,length(pValues));
+msg = rand(length(pValues)*repetitions, 16) > 0.5;
+errors = rand(length(msg), 31) < repmat(reshape(repmat(pValues,repetitions,1),1,[])',1,31);
+bch_encoded = encoder(msg);
 
-for k = 1:length(pValues)
-    p = pValues(k);
-    averageBerrs = 0;
-    for r = 1:repetitions
-        msg = (rand(1, messageLength) > 0.5);
-        bch_encoded = encoder(msg);
-        errors = (rand(1,length(bch_encoded)) < p);
-        bch_decode = berlekamp_decode(mod(bch_encoded + errors, 2));
-        bitErrs = sum(mod(bch_decode + msg,2))/length(msg);
-        averageBerrs = averageBerrs + bitErrs;
-    end
-    BER(k) = (averageBerrs/repetitions);
-end
+bch_decoded = matlabBCHdecode(mod(bch_encoded + errors, 2));
+bitErrs = reshape((sum(mod(bch_decoded' + msg',2))/16)', repetitions, []);
+BER = sum(bitErrs)/length(bitErrs); % take the average
 
 figure()
 plot(pValues, BER);
 title('Plot of BER in BCH codes in a BSC');
-xaxis('P values');
-yaxis('BER');
+xlabel('P values');
+ylabel('BER');
 
 % Q(x) = 0.5 * ( 1 - erf(x/sqrt(2)))
 % Q^(-1)(x) = sqrt(2)*erfinv(1 - 2x)
 
-Xuncoded = 20*log10(1/(sqrt(2)*erfinv(1 - 2.*pValues)));
-Xcoded = Xuncoded - 10*log10(R); % R = code Rate = k/n
+Xuncoded = 20*log10(1./(sqrt(2)*erfinv(1 - 2.*pValues)));
+Xcoded = Xuncoded - 10*log10(16/31); % R = code Rate = k/n
 
 figure()
 plot(Xuncoded, BER, Xcoded, BER, '-r');
 title('Plot of BER in BCH codes in a BSC');
-xaxis('SNR');
-yaxis('BER');
+legend('Uncoded', 'Coded');
+xlabel('SNR');
+ylabel('BER');
 
 % section 1) q 8)b)
 
-BER = zeros(1,length(snrValues));
-
-for k = 1:length(snrValues)
-    s = snrValues(k);
-    averageBerrs = 0;
-    for r = 1:repetitions
-        msg = (rand(1, messageLength) > 0.5);
-        bch_encoded = encoder(msg);
-        signal = 1 - 2*bch_encoded;
-        noise = randn(length(signal))*sqrt(1/s); % TODO: check conversion to snr, put in dB?
-        hard_dec = (signal + noise) < 0; % Hard decision on 0
-        bch_decode = berlekamp_decode(hard_dec);
-        bitErrs = sum(mod(bch_decode + msg,2))/length(msg);
-        averageBerrs = averageBerrs + bitErrs;
-    end
-    BER(k) = (averageBerrs/repetitions);
-end
+% TODO: check conversion to snr, put in dB? sqrt(1./snrValues)?
+msg = rand(length(snrValues)*repetitions, 16) > 0.5;
+bch_encoded = encoder(msg);
+noise = randn(length(bch_encoded), 31).*repmat(reshape(repmat(sqrt(1./snrValues),repetitions,1),1,[])',1,31); 
+signal = 1 - 2*bch_encoded;
+bch_decoded = matlabBCHdecode((signal + noise) < 0); % Hard decision on 0
+bitErrs = reshape((sum(mod(bch_decoded' + msg',2))/16)', repetitions, []);
+BER = sum(bitErrs)/length(bitErrs); % take the average
 
 figure()
 plot(snrValues, BER);
 title('Plot of BER in BCH codes in an AWGN BPSK');
-xaxis('SNR');
-yaxis('BER');
+xlabel('SNR');
+ylabel('BER');
 
--- playAudioOverBSC.m
+++ playAudioOverBSC.m
@@ -2,13 +2,9 @@ function playAudioOverBSC(pVal)
 
 [data, Fs] = wavread('austinpowers.wav', 'native');
 data = de2bi(data);
-data = reshape([data; zeros(1,8)],[],16);
-decoded_data = data;
-for k = 1:length(data);
-    codeword = encoder(double(data(k,:)));
-    errors = rand(1,31) < pVal;
-    decoded_data(k,:) = berlekamp_decode(mod(codeword + errors,2));
-end
-decoded_data = reshape(decoded_data,[], 8);
+data = double(reshape([data; zeros(1,8)],[],16));
+errors = rand(length(data),31) < pVal;
+encoded_data = mod(encoder(data) + errors,2);
+decoded_data = reshape(matlabBCHdecode(encoded_data),[], 8);
 decoded_data = bi2de(decoded_data(1:end-1,:)); % remove the last row of zeros again
 soundsc(double(decoded_data),Fs);
