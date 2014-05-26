b688e24b1bc747584f69dcc5d041aff23ab4032a
Stephen Tridgell <stephen@tridgell.net>
2014-05-26 18:28:28 +1000

    Misread the question, had to plot uncoded too. Small changes to report

--- bch_ber_simulation.m
+++ bch_ber_simulation.m
@@ -1,6 +1,6 @@
 % simulations section 1) part 8)
 % Find the BER over a BCH code
-pValues = 0.001:0.001:0.5;
+pValues = 0.001:0.001:0.35;
 snrValues = -5:0.001:10;
 repetitions = 500;
 
@@ -45,9 +45,18 @@ bch_decoded = matlabBCHdecode((signal + noise) < 0); % Hard decision on 0
 bitErrs = reshape((sum(mod(bch_decoded' + msg',2))./16)', repetitions, []);
 BER = sum(bitErrs)/length(bitErrs); % take the average
 
+noise = randn(length(msg), 16).* ...
+      repmat(reshape(repmat(10.^(-snrValues./20),repetitions,1) ...
+             ,1,[])',1,16); 
+signal = 1 - 2.*msg;
+decoded = (signal + noise) < 0;
+bitErrs = reshape((sum(mod(decoded' + msg',2))./16)', repetitions, []);
+BER2 = sum(bitErrs)/length(bitErrs); % take the average
+
 figure()
-plot(snrValues, BER);
+plot(snrValues, [BER2; BER]);
 title('Plot of BER in BCH codes in an AWGN BPSK');
+legend('Uncoded', 'Coded');
 xlabel('SNR');
 ylabel('BER');
 
