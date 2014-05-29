d4ae37e5355d898d91a69b738c0bb1901b1713c9
unknown <stri1875@WGH546Z1.internal.usyd.edu.au>
2014-05-29 16:29:21 +1000

    Updated plots with better images

--- bch_ber_simulation.m
+++ bch_ber_simulation.m
@@ -1,8 +1,7 @@
 % simulations section 1) part 8)
 % Find the BER over a BCH code
-pValues = 0.001:0.001:0.35;
-snrValues = -5:0.001:10;
-repetitions = 500;
+pValues = [0.005 0.002 0.001];
+repetitions = 10000000;
 
 msg = rand(length(pValues)*repetitions, 16) > 0.5;
 errors = rand(length(msg), 31) < ...
@@ -12,10 +11,15 @@ bch_encoded = encoder(msg);
 bch_decoded = matlabBCHdecode(mod(bch_encoded + errors, 2));
 bitErrs = reshape((sum(mod(bch_decoded' + msg',2))/16)', repetitions, []);
 BER = sum(bitErrs)/length(bitErrs); % take the average
+errors = rand(length(msg), 16) < ...
+        repmat(reshape(repmat(pValues,repetitions,1),1,[])',1,16);
+bitErrs = reshape((sum(mod(errors',2))./16)', repetitions, []);
+BERUncoded = sum(bitErrs)/length(bitErrs);
 
 figure()
-semilogy(pValues, BER);
+semilogy(fliplr(pValues), fliplr([BERUncoded; BER]));
 title('Plot of BER in BCH codes in a BSC');
+legend('Uncoded', 'Coded');
 ylim([10^-6 10^0]);
 xlabel('P values');
 ylabel('BER');
@@ -27,14 +31,18 @@ Xuncoded = 20.*log10(sqrt(2).*erfinv(1 - 2.*pValues));
 Xcoded = Xuncoded - 10.*log10(16/31); % R = code Rate = k/n
 
 figure()
-semilogy([Xuncoded; Xcoded], BER);
+semilogy(Xuncoded, BERUncoded, Xcoded, BER, '-g');
 title('Plot of BER in BCH codes in a BSC');
 legend('Uncoded', 'Coded');
 ylim([10^-6 10^0]);
 xlabel('SNR');
 ylabel('BER');
 
+clear all; % otherwise run out of memory
 % section 1) q 8)b)
+
+snrValues = 0:0.01:8;
+repetitions = 5000;
 
 % SNR = 10*log10((1/ampl)^2), ampl = 10^(-SNR/20)
 msg = rand(length(snrValues)*repetitions, 16) > 0.5;
